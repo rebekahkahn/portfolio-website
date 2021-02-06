11210a5b6ae09ae87d81cbd2661a8cb1ce3cc0ae
Rebekah Kahn <rebekah.m.kahn@gmail.com>
2021-02-06 13:17:43 -0500

    added switch (no functionality yet)

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 This project was primarily created with [Create React App](https://github.com/facebook/create-react-app) and [material-ui](https://material-ui.com/)
 
-## Done (Feb 5, 2021)
+## Done (Feb 6, 2021)
 
-- Commited fresh create-react-app project to gihub
-- Yarn added material-ui and font-awesome
-- Added appbar and a jumbotron-like components
+- Added switch (later will toggle dark and light theme)
 
 ## To Do (shorter term)
 
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,14 +3,30 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import Switch from "@material-ui/core/Switch";
 
 function Header() {
   const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.checked });
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appbarColor}>
         <Toolbar>
+          <div className={classes.switch}>
+            <Switch
+              checked={state.checkedA}
+              onChange={handleChange}
+              name="checkedA"
+            />
+          </div>
           <div className={classes.buttonGroup}>
             <Button color="inherit">About</Button>
             <Button color="inherit">Projects</Button>
@@ -36,6 +52,10 @@ const useStyles = makeStyles((theme) => ({
   buttonGroup: {
     position: "fixed",
     right: "10px",
+  },
+  switch: {
+    position: "fixed",
+    left: "10px",
   },
 }));
 
