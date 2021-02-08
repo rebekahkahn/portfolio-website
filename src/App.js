d3d8e72acf8d0c014cfdd8b79f8a14b4243aedb6
Rebekah Kahn <rebekah.m.kahn@gmail.com>
2021-02-07 21:23:38 -0500

    added themeprovider and cssbaseline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React from "react";
+import Main from "./components/MainComponent";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Main />
+  </ThemeProvider>
+);
 export default App;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <p>Here is the Main Component</p>;
+};
+
+export default Main;
